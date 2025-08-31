0be4f7b784d18aaa079bde0ea2f5670d73fb7e4c
ilhamitto1 <dosdiyevilham@gmail.com>
2025-08-31 12:29:03 +0400

    Initial commit: add personal portfolio project

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,60 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/profile1.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Ilham Dosdiyev"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Ilham
+          <br />
+          Dosdiyev
+        </h1>
+        <h2>Software Developer</h2>
+        <span>
+          <a href="https://github.com/ilhamitto1" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/ilham-dosdiyev-b27a09305/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+       Software Developer with a strong interest in building scalable and efficient systems by combining expertise in Data Engineering and Frontend Development. Passionate about transforming data into actionable insights and creating seamless digital experiences with modern web technologies
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
